7f6050ea4237a8d6e63ec8393d21f6c001af1bc8
HagenSV <HagenSV22@gcc.edu>
2024-05-02 15:30:13 -0400

    Added more comments

--- src/main/java/GraphicsWindow.java
+++ src/main/java/GraphicsWindow.java
@@ -23,11 +23,13 @@ public class GraphicsWindow extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
+        // Converts graphics into more powerful graphics
         Graphics2D g2 = (Graphics2D) g;
 
-        //Draw stuff here
+        // Translates the graphics space so that 0,0 is in the center of the screen
         g2.translate(getWidth()/2,getHeight()/2);
 
+        // Draw Objects
         g2.setColor(Color.YELLOW);
         g2.fillOval(-25,-25,50,50);
 
@@ -36,7 +38,16 @@ public class GraphicsWindow extends JPanel {
 
     }
 
+    /**
+     * Draws text centered on (x,y)
+     * Adapted from https://stackoverflow.com/questions/27706197/how-can-i-center-graphics-drawstring-in-java
+     * @param g the graphics context to draw on
+     * @param text the string to render
+     * @param x center x coordinate
+     * @param y center y coordinate
+     */
     private void drawTextCentered(Graphics2D g, String text, int x, int y){
+
         // Get the FontMetrics
         FontMetrics metrics = g.getFontMetrics(g.getFont());
 
