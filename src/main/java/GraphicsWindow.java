51271c1aff6f73847cfca4e1aa6e04a57f1645f3
HagenSV <HagenSV22@gcc.edu>
2024-05-03 16:54:13 -0400

    Camera movement and scaling

--- src/main/java/GraphicsWindow.java
+++ src/main/java/GraphicsWindow.java
@@ -1,10 +1,21 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class GraphicsWindow extends JPanel {
 
+    private float cameraX = 30;
+    private float cameraY = 0;
+    private boolean forward, backward, left, right;
+    private static final float CAMERA_MOVE_SPEED = 5f;
+    private float scale = 1;
+
+    private long lastRepaint = System.currentTimeMillis();
     public static void main(String[] args) {
-        new GraphicsWindow();
+        GraphicsWindow w = new GraphicsWindow();
+        while (true){
+            w.repaint();
+        }
     }
     public GraphicsWindow(){
         JFrame frame = new JFrame("N-body simulation");
@@ -17,21 +28,49 @@ public class GraphicsWindow extends JPanel {
         setBackground(Color.BLACK);
         setFocusable(true);
         requestFocus();
+
+        addKeyListener(new KeyInput());
+        addMouseWheelListener(new MouseWheelInput());
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
+        long currentTime = System.currentTimeMillis();
+        float deltaTime = (currentTime-lastRepaint)*0.01f;
+        lastRepaint = currentTime;
+
+        if (forward) cameraY -= CAMERA_MOVE_SPEED*deltaTime/scale;
+        if (backward) cameraY += CAMERA_MOVE_SPEED*deltaTime/scale;
+        if (left) cameraX -= CAMERA_MOVE_SPEED*deltaTime/scale;
+        if (right) cameraX += CAMERA_MOVE_SPEED*deltaTime/scale;
+
+
+        //TODO get info from objects
+        float scaledX = 0*scale;
+        float scaledY = 0*scale;
+        float scaledR = 50*scale;
+
         // Converts graphics into more powerful graphics
         Graphics2D g2 = (Graphics2D) g;
 
         // Translates the graphics space so that 0,0 is in the center of the screen
-        g2.translate(getWidth()/2,getHeight()/2);
+        g2.translate( getWidth()/2-(cameraX*scale), getHeight()/2-(cameraY*scale) );
 
         // Draw Objects
         g2.setColor(Color.YELLOW);
-        g2.fillOval(-25,-25,50,50);
+
+        g2.translate( scaledX-scaledR , scaledY-scaledR );
+
+        g2.fillOval(
+                (int)(0),
+                (int)(0),
+                (int)(2*scaledR),
+                (int)(2*scaledR)
+        );
+
+        g2.translate( scaledR-scaledX, scaledR-scaledY );
 
         g2.setColor(Color.BLACK);
         drawTextCentered(g2,"The Sun",0,0);
@@ -48,6 +87,8 @@ public class GraphicsWindow extends JPanel {
      */
     private void drawTextCentered(Graphics2D g, String text, int x, int y){
 
+        //g.getFont().getSize();
+
         // Get the FontMetrics
         FontMetrics metrics = g.getFontMetrics(g.getFont());
 
@@ -59,5 +100,51 @@ public class GraphicsWindow extends JPanel {
 
         // Draw the String
         g.drawString(text, centerX, centerY);
+    }
+
+    private class KeyInput extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            switch (e.getKeyCode()){
+                case KeyEvent.VK_W:
+                    forward = true;
+                    break;
+                case KeyEvent.VK_S:
+                    backward = true;
+                    break;
+                case KeyEvent.VK_A:
+                    left = true;
+                    break;
+                case KeyEvent.VK_D:
+                    right = true;
+            }
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            switch (e.getKeyCode()){
+                case KeyEvent.VK_W:
+                    forward = false;
+                    break;
+                case KeyEvent.VK_S:
+                    backward = false;
+                    break;
+                case KeyEvent.VK_A:
+                    left = false;
+                    break;
+                case KeyEvent.VK_D:
+                    right = false;
+            }
+        }
+    }
+
+
+    private class MouseWheelInput implements MouseWheelListener {
+        @Override
+        public void mouseWheelMoved(MouseWheelEvent e) {
+            System.out.println(e.getPreciseWheelRotation());
+            scale += (float)(e.getPreciseWheelRotation());
+            scale = Math.max(scale,0.25f);
+        }
     }
 }
